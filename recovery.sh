dd6af19b88377abb806da6e1f952ed235222de27
Jeff Freedman <freedman.jeffrey@gmail.com>
2019-03-31 12:55:43 -0400

    Update recovery.sh

--- recovery.sh
+++ recovery.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 #This is a recovery file to start the hotspot regardless of what you had previously set up
+    ifconfig "$wifidev" down  #added
+    ifconfig "$wifidev" up #added
+
+    ip link set dev "$wifidev" down
+    ip a add 192.168.4.1/24 brd + dev "$wifidev"
+    ip link set dev "$wifidev" up
+    dhcpcd -k "$wifidev" >/dev/null 2>&1
+    iptables -t nat -A POSTROUTING -o "$ethdev" -j MASQUERADE
+    iptables -A FORWARD -i "$ethdev" -o "$wifidev" -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i "$wifidev" -o "$ethdev" -j ACCEPT
+    systemctl start dnsmasq
+    systemctl start hostapd
+    echo 1 > /proc/sys/net/ipv4/ip_forward
